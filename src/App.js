8e5a272eb93fe189b7309e21f4617226efc2aea1
Arion Vargas <arionvargas07@live.com.ar>
2023-11-27 23:04:30 -0300

    segunda pre entrega

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css'
 import NavBar from "./Components/NavBar/NavBar"
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer'
 import "bulma/css/bulma.css"
+import ItemCount from './Components/ItemCount/ItemCount'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <ItemListContainer greeting={"Bienvenidos a mi tienda!!"}/>
+      <ItemCount initial={1} stock={15} onAdd={(quantity) => console.log("cantidad agregada" ,quantity)}/>
     </div>
   )
 }
--- src/Components/CartWidget/CartWidget.js
+++ src/Components/CartWidget/CartWidget.js
@@ -1,12 +1,20 @@
+import { useState } from "react"
 import cart from "./assets/carrito.png"
 
 const CartWidget = () => {
+
+    const [count, setCount] = useState(0)
+    function handleClick() {
+        setCount(count + 1)
+    }
+
     return (
         <div>
-            <img src={cart} alt="img-carrito" />
-            0
-        </div>
 
+            <img src={cart} alt="img-carrito" />
+           
+            {count}
+        </div>
     )
 }
 
